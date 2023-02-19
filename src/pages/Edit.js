ce9f3c45fe2ad05a51ed83aebd6e3618f16c6159
Jieun Pivarnik <JPivarnik@jis-macbook-pro.lan>
2023-02-19 17:57:45 -0600

    edit page

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,7 +1,91 @@
-function Edit() {
-    return (
-        <div className="edit">Edit</div>
-    )
+import {useParams, useNavigate} from 'react-router-dom'
+import {useState} from 'react'
+
+
+function Edit(props) {
+    const {id} = useParams();
+    const food = props.food.find((food)=>food.id === +id)
+    let navigate = useNavigate()
+
+    const[editForm, setEditForm] = useState(food)
+
+    const handleChange =(event) =>{
+        setEditForm((prevState) => ({
+             ...prevState, [event.target.name]: event.target.value,
+        }))
+        console.log(event.target.value)
+    }
+
+    const handleSubmit = (event)=>{
+        event.preventDefault()
+        props.updateTopics(editForm, food.id)
+        console.log(editForm)
+        navigate("/foodmood")
+    }
+    const removeTopic = () => {
+        props.deleteTopics(food._id)
+        navigate("/foodmood")
+    }
+
+    return(
+        <section> 
+          <form onSubmit={handleSubmit}>
+            <div>
+              Mood:<input
+              type="text"
+              value={editForm.mood}
+              name="mood"
+              placeholder="topic"
+              onChange={handleChange}
+            />
+            </div>
+            <div>
+              Food:<input
+              type="text"
+              value={editForm.name}
+              name="food"
+              placeholder="food"
+              onChange={handleChange}
+            /> 
+            </div>
+            
+            <div>
+              Image:<input
+              type="text"
+              value={editForm.img}
+              name="image"
+              placeholder="image"
+              onChange={handleChange}
+            />  
+            </div>
+            
+            <div>
+              Info:<input
+              type="text"
+              value={editForm.info}
+              name="Information"
+              placeholder="Information"
+              onChange={handleChange}
+            />
+            </div>
+            
+            <div>
+              Recipe: <input
+              className="input"
+              type="text"
+              value={editForm.recipe}
+              name="recipe"
+              placeholder="receipe link"
+              onChange={handleChange}
+            /> 
+            </div>
+           
+            <div><input id = "update-button"className="input" type="submit" value="Update"/></div>
+            <button id="delete-button" className="input button" onClick = {()=>removeTopic(food.id)}>Delete</button>
+
+          </form>
+        </section>
+      )
 }
 
 export default Edit
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,7 +1,30 @@
-function Show() {
-    return (
-        <div className="show">Show</div>
-    )
+
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom"
+
+
+function Show(props) {
+
+  const { id } = useParams()
+  const food = props.food.find((food) => food.id === +id)
+  console.log(props.food)
+
+  return (
+    <div>
+      <div >
+        <h1>If you are feeling {food.mood} ...</h1>
+        <img id = "image" src={food.img} alt={food.food} /><br/>
+        <p><span id="click">Click here for recipe </span><br/></p>
+           
+      </div>
+      
+      
+     
+      <Link to={`/foodmood/${food.id}/edit`}><button id="edit-button" className="input button">Edit</button></Link>
+
+    </div>
+  )
+
 }
 
-export default Show
+export default Show;
