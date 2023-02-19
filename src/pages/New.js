9d8398d78aaa6d0973fce68a071d06f425a7b306
Matthew <subinmatt@gmail.com>
2023-02-19 15:52:38 -0800

    new page form completed

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,7 +1,84 @@
-function New() {
-    return (
-        <div className="new">New</div>
-    )
+import { useState } from "react";
+import {useNavigate} from 'react-router-dom'
+
+
+function New(props) {
+  let navigate =  useNavigate()
+  const [newForm, setNewForm] = useState({
+    name: "",
+    mood: "",
+    img: "",
+    info: "",
+    recipe: "",
+  });
+
+  const handleChange = (event) => {
+    setNewForm((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.createTopics(newForm);
+    setNewForm({
+        name: "",
+        mood: "",
+        img: "",
+        info: "",
+        recipe: "",
+    });
+    navigate("/foodmood")
+  };
+  return (
+    <section className= "content-margin">
+      <form onSubmit={handleSubmit}>
+        <input
+          className='name'
+          type="text"
+          value={newForm.name}
+          name="name"
+          placeholder="name"
+          onChange={handleChange}
+        />
+        <input
+          className='input'
+          type="text"
+          value={newForm.mood}
+          name="mood"
+          placeholder="mood"
+          onChange={handleChange}
+        />
+        <input
+          className='input'
+          type="text"
+          value={newForm.img}
+          name="img"
+          placeholder="img"
+          onChange={handleChange}
+        />
+        <input
+          className='input'
+          type="text"
+          value={newForm.info}
+          name="info"
+          placeholder="info"
+          onChange={handleChange}
+        />
+        <input
+          className='input'
+          type="text"
+          value={newForm.recipe}
+          name="recipe"
+          placeholder="recipe"
+          onChange={handleChange}
+        />
+        <br/>
+        <input id="add-button" className='input button' type="submit" value="Add" />
+      </form>
+    </section>
+  );
 }
 
-export default New
+export default New;
