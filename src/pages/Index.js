9cd0d821bd159b48c4a63c9c89e1ab85f298b9de
Cody Clark <codylcl@yahoo.com>
2023-02-19 16:57:37 -1000

    routes complete

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -10,26 +10,69 @@ function Index(props) {
         info: foodItem.info,
       };
     });
-    console.log(foodArray);
 
     const happyFoodArray = foodArray.filter((foodItem) => {
       return foodItem.mood.toLowerCase().includes("happy".toLowerCase());
     });
-    console.log(happyFoodArray);
+
+    const stressedFoodArray = foodArray.filter((foodItem) => {
+      return foodItem.mood.toLowerCase().includes("stressed".toLowerCase());
+    });
+
+    const sadFoodArray = foodArray.filter((foodItem) => {
+      return foodItem.mood.toLowerCase().includes("sad".toLowerCase());
+    });
+
+    const lazyFoodArray = foodArray.filter((foodItem) => {
+      return foodItem.mood.toLowerCase().includes("lazy".toLowerCase());
+    });
+
+    const boredFoodArray = foodArray.filter((foodItem) => {
+      return foodItem.mood.toLowerCase().includes("bored".toLowerCase());
+    });
+
+    const healthyFoodArray = foodArray.filter((foodItem) => {
+      return foodItem.mood.toLowerCase().includes("healthy".toLowerCase());
+    });
 
     function randomizeFromArray(arr) {
       const randomIndex = Math.floor(Math.random() * arr.length);
       return arr[randomIndex];
     }
-    const randomFoodItem = randomizeFromArray(happyFoodArray);
-    console.log(randomFoodItem);
+    const randomHappyItem = randomizeFromArray(happyFoodArray);
+
+    const randomStressedItem = randomizeFromArray(stressedFoodArray);
+
+    const randomSadItem = randomizeFromArray(sadFoodArray);
+
+    const randomLazyItem = randomizeFromArray(lazyFoodArray);
+
+    const randomBoredItem = randomizeFromArray(boredFoodArray);
+
+    const randomHealthyItem = randomizeFromArray(healthyFoodArray);
+
 
     return (
       <div>
         <h3 className="title">I am feeling today... </h3>
         <div>
-          <Link to={`/foodmood/${randomFoodItem?.id}`}>
-            <p>HELP US KATIE</p>
+          <Link to={`/foodmood/${randomHappyItem?.id}`}>
+            <p>Happy</p>
+          </Link>
+          <Link to={`/foodmood/${randomStressedItem?.id}`}>
+            <p>Stressed</p>
+          </Link>
+          <Link to={`/foodmood/${randomSadItem?.id}`}>
+            <p>Sad</p>
+          </Link>
+          <Link to={`/foodmood/${randomLazyItem?.id}`}>
+            <p>Lazy</p>
+          </Link>
+          <Link to={`/foodmood/${randomBoredItem?.id}`}>
+            <p>Bored</p>
+          </Link>
+          <Link to={`/foodmood/${randomHealthyItem?.id}`}>
+            <p>Healthy</p>
           </Link>
         </div>
       </div>
