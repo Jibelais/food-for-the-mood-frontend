81d5f2bbbacc8e3ebf69cb3bb0a39b29ba62720b
Jieun Pivarnik <JPivarnik@jis-macbook-pro.lan>
2023-02-20 13:50:12 -0600

    style index-complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,15 @@
+import { Link } from "react-router-dom";
 function Header() {
     return (
-        <div className="header">Header</div>
+        <header className="header">
+            <Link to={`/foodmood`}><img id = "logo" src="/img/logo.png"alt="icon"/></Link>
+
+            <nav>
+            <Link to={`/`}>Home</Link>
+            <Link to={`/foodmood`}>Mood</Link>
+            <Link to={`/lessisgreener/add`}>Add Ideas</Link>
+            </nav>
+        </header>
     )
 }
 
