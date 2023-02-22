98dfedd4a978fae87f295c0371c44115a6de68a8
Cody Clark <codylcl@yahoo.com>
2023-02-21 14:58:07 -1000

    about page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,20 @@
 function Home() {
     return (
-        <div className="home">Home</div>
-    )
-}
-
-export default Home
+      <div className="about">
+        <h1>About</h1>
+        <img src="https://thumbs.dreamstime.com/b/food-variety-panorama-shot-above-selection-healthy-foods-meat-fish-fruit-vegetables-concept-balanced-diet-207809870.jpg"></img>
+        <p>
+          OMG what do I want to eat?! Have you ever felt hangry with no idea what
+          to eat. Food to the Mood is here to solve your indecisiveness! Our dream
+          is to help you find the perfect food for how you are feeling. We have a
+          variety of foods that will randomly be selected each time you click a
+          mood. They each come with their own matching reason for the mood.
+          Sometimes we eat foods and just dont know why they might make us feel
+          better. We wanted to cure your hangriness and explain the magic behind
+          it!{" "}
+        </p>
+      </div>
+    );
+  }
+  
+  export default Home;
