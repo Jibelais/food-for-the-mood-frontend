dd521cf20a5b3ec628ed78d1d0ca7137c62f5f70
Jieun Pivarnik <JPivarnik@jis-macbook-pro.lan>
2023-02-21 22:21:26 -0600

    fixed link

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 function Header() {
     return (
         <header className="header">
-            <Link to={`/foodmood`}><img id = "logo" src="/img/logo.png"alt="icon"/></Link>
+            <Link to={`/`}><img id = "logo" src="/img/logo.png"alt="icon"/></Link>
 
             <nav>
-            <Link to={`/`}>About</Link>
-            <Link to={`/foodmood`}>Mood</Link>
+            <Link to={`/foodmood/about`}>About</Link>
+            <Link to={`/`}>Mood</Link>
             <Link to={`/foodmood/new`}>Add Food</Link>
             <Link to={`/foodmood/list`}>List</Link>
             </nav>
